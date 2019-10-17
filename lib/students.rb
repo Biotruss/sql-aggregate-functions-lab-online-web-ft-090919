30f87f8c9145a5e29b2ea07180bb5c7977e1bd37
Craig Christopher Messi <craig.messi@gmail.com>
2019-10-17 07:15:40 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,14 +1,15 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
-ef highest_student_gpa
-  SELECT gpa FROM students ORDER BY gpa LIMIT 1;
+def highest_student_gpa
+  "SELECT MAX(gpa) as highest_gpa FROM students;"
 end
 
 def lowest_student_gpa
-  SELECT gpa FROM students ORDER BY gpa DESC LIMIT 1;
+  "SELECT MIN(GPA) AS lowest_gpa FROM studenets;"
 end
 
 def average_student_gpa
+  "SELECT "
 end
 
 def total_tardies_for_all_students
