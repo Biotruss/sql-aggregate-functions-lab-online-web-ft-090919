c9351ebf60d683a193be41d930b38df5cd647c17
Craig Christopher Messi <craig.messi@gmail.com>
2019-10-17 07:18:33 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,14 +5,15 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  "SELECT MIN(GPA) AS lowest_gpa FROM studenets;"
+  "SELECT MIN(gpa) AS lowest_gpa FROM students;"
 end
 
 def average_student_gpa
-  "SELECT "
+  "SELECT AVG(gpa) AS average_gpa FROM students;"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(tardies) AS "
 end
 
 def average_gpa_for_9th_grade
